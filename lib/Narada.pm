b03b960f3844fb68e77eb5f354a516b90b067af4
Alex Efros <powerman@cpan.org>
2013-05-12 16:05:52 +0000

    version 1.3.6

--- lib/Narada.pm
+++ lib/Narada.pm
@@ -3,7 +3,7 @@ package Narada;
 use warnings;
 use strict;
 
-use version; our $VERSION = qv('1.3.5');
+use version; our $VERSION = qv('1.3.6');
 
 1; # Magic true value required at end of module
 __END__
@@ -15,7 +15,7 @@ Narada - framework for ease development/deploy/support for medium/big projects
 
 =head1 VERSION
 
-This document describes Narada version 1.3.5
+This document describes Narada version 1.3.6
 
 
 =head1 SYNOPSIS
