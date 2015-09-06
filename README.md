8e10fef7af20ba00cfbf48c9e58e8cb469e18612
Alex Efros <powerman@powerman.name>
2015-09-06 14:58:30 +0300

    v2.2.1

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Narada - framework for ease deploy and support microservice projects
 
 # VERSION
 
-This document describes Narada version v2.2.0
+This document describes Narada version v2.2.1
 
 # SYNOPSIS
 
--- lib/Narada/Lock.pm
+++ lib/Narada/Lock.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 use Carp;
 
-our $VERSION = 'v2.2.0';
+our $VERSION = 'v2.2.1';
 
 use Perl6::Export::Attrs;
 use Narada;
@@ -86,7 +86,7 @@ Narada::Lock - manage project locks
 
 =head1 VERSION
 
-This document describes Narada::Lock version v2.2.0
+This document describes Narada::Lock version v2.2.1
 
 
 =head1 SYNOPSIS
